5944bf500a8da21fd8239c42d071e18fbb26463c
gstheo <giorgostheo97@gmail.com>
2023-04-27 12:28:11 +0300

    docker related

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cargo clean
 # docker build -t marshal .
 docker build -t marshal .
-docker run --rm -it --entrypoint bash marshal
-# docker run --rm marshal cargo run --release > result.txt
+# docker run --rm -it --entrypoint bash marshal
+docker run --rm marshal cargo run --release > result.txt
